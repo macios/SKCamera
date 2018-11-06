4396cb14ef7e2ca510eb40b77ef7b88b460eab0d
ac-hu <hunanlinmac@163.com>
2018-11-06 10:33:26 +0800

    111

---
+++ SKCamera/SKCamera/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SKCamera
+//
+//  Created by ac-hu on 2018/11/6.
+//  Copyright © 2018年 SK-HU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
